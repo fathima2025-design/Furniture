43dbad09eff187e556df679316ddd58f0be1e27c
fathima <danishahamed2023@gmail.com>
2025-06-23 14:03:15 +0530

    initial commit

---
+++ angularbasics/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HerosectionComponent } from "../../furniture/herosection/herosection.component";
+import { ProductComponent } from "../../furniture/product/product.component";
+import { HeaderComponent } from '../../furniture/header/header.component';
+
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [RouterModule, HerosectionComponent, ProductComponent,HeaderComponent]
+})
+export class AppComponent {
+  title = 'angularbasics';
+}
